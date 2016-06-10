365eb257819e8fbc09282fb8e8110a5410ad669e
vvolkov <vitaliy.v@tradingview.com>
2016-06-10 09:28:04 +0300

    Added functions with E suffix (without panic)

--- config.go
+++ config.go
@@ -4,8 +4,6 @@ import (
 	"encoding/json"
 	"os"
 	"fmt"
-	"reflect"
-	"github.com/spf13/cast"
 )
 
 type Config struct {
@@ -33,55 +31,95 @@ func (this *Config) LoadE(jsonFilePath string) error {
 func (this *Config) Load(jsonFilePath string) {
 	var err = this.LoadE(jsonFilePath)
 	if err != nil {
-		panic(fmt.Sprintf("Cannot load config file %s, reason: %v", jsonFilePath, err))
+		panic(fmt.Sprintf("Cannot load config file '%s', reason: %v", jsonFilePath, err))
 	}
 }
 
-func (this *Config) getValueOrDefault(key string) interface{} {
-	var val, ok = this.values[key]
+func (this *Config) getValueOrDefaultE(key string) (interface{}, error) {
+	val, ok := this.values[key]
 	if !ok {
 		val, ok = this.defaults[key]
 		if !ok {
-			panic(fmt.Sprintf("Key %s is not found in config nor in configDefaults!", key))
+			return nil, fmt.Errorf("Key '%s' is not found in config.json nor config defaults", key)
 		}
 	}
-	return val
+	return val, nil
+}
+
+func (this *Config) GetStrE(key string) (res string, err error) {
+	val, err := this.getValueOrDefaultE(key)
+	if err != nil {
+		return res, err
+	}
+	res, ok := val.(string)
+	if !ok {
+		return res, fmt.Errorf("Type assertion '%v' to string failed", val)
+	}
+	return res, nil
 }
 
 func (this *Config) GetStr(key string) string {
-	var val = this.getValueOrDefault(key)
-	var res, err = cast.ToStringE(val)
+	res, err := this.GetStrE(key)
 	if err != nil {
-		panic(fmt.Sprintf("%v is not a string! It is %v", val, reflect.TypeOf(val)))
+		panic(err)
 	}
 	return res
+}
+
+func (this *Config) GetIntE(key string) (res int, err error) {
+	val, err := this.GetFloat64E(key) // NOTE: JSON stores any number as float
+	if err != nil {
+		return res, fmt.Errorf("Could not get number value for key '%s', reason: %v", key, err)
+	}
+	res = int(val) // TODO: panic if fractional part is not zero
+	return res, nil
 }
 
 func (this *Config) GetInt(key string) int {
-	var val = this.getValueOrDefault(key)
-	// NOTE: JSON stores numbers as floats
-	var res, err = cast.ToIntE(val)
+	res, err := this.GetIntE(key)
 	if err != nil {
-		panic(fmt.Sprintf("%v is not an int number! It is %v", val, reflect.TypeOf(val)))
+		panic(err)
 	}
 	return res
+}
+
+func (this *Config) GetFloat64E(key string) (res float64, err error) {
+	val, err := this.getValueOrDefaultE(key)
+	if err != nil {
+		return res, err
+	}
+	res, ok := val.(float64)
+	if !ok {
+		return res, fmt.Errorf("Type assertion '%v' to float failed", val)
+	}
+	return res, nil
 }
 
 func (this *Config) GetFloat64(key string) float64 {
-	var val = this.getValueOrDefault(key)
-	var res, err = cast.ToFloat64E(val)
+	res, err := this.GetFloat64E(key)
 	if err != nil {
-		panic(fmt.Sprintf("%v is not a float number! It is %v", val, reflect.TypeOf(val)))
+		panic(err)
 	}
 	return res
 }
 
 
-func (this *Config) GetBool(key string) bool {
-	var val = this.getValueOrDefault(key)
-	var res, err = cast.ToBoolE(val)
+func (this *Config) GetBoolE(key string) (res bool, err error) {
+	val, err := this.getValueOrDefaultE(key)
 	if err != nil {
-		panic(fmt.Sprintf("%v is not a bool value! It is %v", val, reflect.TypeOf(val)))
+		return res, err
+	}
+	res, ok := val.(bool)
+	if !ok {
+		fmt.Errorf("Type assertion '%v' to bool failed", val)
+	}
+	return res, nil
+}
+
+func (this *Config) GetBool(key string) bool {
+	var res, err = this.GetBoolE(key)
+	if err != nil {
+		panic(err)
 	}
 	return res
 }
